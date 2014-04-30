5b57577f8354e08633e2465e1c09fdc3a7e5beee
AniwaysIlona <ilona@aniways.com>
2014-04-30 10:33:29 +0300

    packs index page and style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,10 @@ Pack.delete_all
 	style = Style.create(name: "s_#{index}")
 	style.packs.create(name:"p_#{index}")
 	style.packs.create(name:"p_#{index}_2")
+	style.packs.create(name:"p_#{index}_3")
+	style.packs.create(name:"p_#{index}_4")
+	style.packs.create(name:"p_#{index}_5")
+	style.packs.create(name:"p_#{index}_6")
+	style.packs.create(name:"p_#{index}_7")
+	style.packs.create(name:"p_#{index}_8")
 end
