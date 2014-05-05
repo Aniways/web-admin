9aa2adf03647d2a26bca8e75f24c94eb1cd0391a
Daniel Kalman <daniel@aniways.com>
2014-05-05 10:01:11 +0300

    Added Validation for category presence in style

--- app/models/style.rb
+++ app/models/style.rb
@@ -1,6 +1,9 @@
 class Style < ActiveRecord::Base
+
   enum category: [ :Feelings, :FoodAndDrinks, :Games, :Nature, :Home, :Brands ]
   has_many :packs
+
   validates :name, uniqueness: true, presence: true
+  validates_presence_of :category
 
 end
