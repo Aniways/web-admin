61e6df74ef40898d783d149fc11358062ece550b
Daniel Kalman <daniel@aniways.com>
2014-05-05 10:00:41 +0300

    Select category from selector in style view

--- spec/features/style_page_spec.rb
+++ spec/features/style_page_spec.rb
@@ -4,9 +4,9 @@ feature 'styles index page' do
   scenario 'Designer enters the main page' do
     style_name = 'my_style'
     visit root_path
-    #let(:my_style) { create(:style)}
     within(".simple_form") do
       fill_in 'style[name]', :with => style_name
+      all('#style_category option')[1].select_option
       find('.btn-primary').click
     end
     expect(page).to have_css('h1', text: 'STYLES')
