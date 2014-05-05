983b6df039fa4aca5e10a214a72672d2d42b1371
Daniel Kalman <daniel@aniways.com>
2014-05-05 10:00:08 +0300

    Set category in style factory

--- spec/factories/models_factory.rb
+++ spec/factories/models_factory.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :style do
+    category 'Feelings'
     sequence(:name) {|n| "style_#{n}" }
   end
 
@@ -11,7 +12,7 @@ FactoryGirl.define do
   factory :pack do
     sequence(:name) {|n| "pack_#{n}" }
     style
-    
+
     ignore do
       icons_count 5
     end
